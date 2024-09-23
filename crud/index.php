8c860625b1a67815d238f37439cfdb293ba570fc
henrique0927 <henrique_stolle@estudante.sesisenai.org.br>
2024-09-23 12:41:12 -0300

    botão de excluir novo

--- crud/index.php
+++ crud/index.php
@@ -13,13 +13,13 @@ if (isset($_POST['create'])) {
     $stmt->execute();
     $professor_id = $conn->insert_id;
 
-    // Inserir nova sala na tabela "aulas"
+    // inserir nova sala na tabela "aulas"
     $stmt = $conn->prepare("INSERT INTO aulas (sala) VALUES (?)");
     $stmt->bind_param("s", $sala);
     $stmt->execute();
     $aula_id = $conn->insert_id;
 
-    // Inserir na tabela "dia_hora"
+    // inserir na tabela "dia_hora"
     $stmt = $conn->prepare("INSERT INTO dia_hora (professor_id, aula_id, hora_aula) VALUES (?, ?, ?)");
     $stmt->bind_param("iis", $professor_id, $aula_id, $hora_aula);
     $stmt->execute();
@@ -28,7 +28,7 @@ if (isset($_POST['create'])) {
     exit();
 }
 
-// Lógica para atualização
+// UPDATE
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
     $nome_professor = $_POST['nome_professor'];
@@ -101,7 +101,7 @@ if (isset($_GET['delete'])) {
             </thead>
             <tbody>
                 <?php
-                // puxar os pedidos "READ"
+                // puxar os pedidos READ
                 $sql = "SELECT dh.id, p.nome_professor, a.sala, dh.hora_aula 
                         FROM dia_hora dh 
                         JOIN professores p ON dh.professor_id = p.professor_id 
@@ -125,7 +125,46 @@ if (isset($_GET['delete'])) {
                             <div id="botao-junto">
                                 <button type="submit" name="update">Atualizar</button>
                                 <!-- botão para excluir pedido -->
-                                <a href="index.php?delete=<?php echo htmlspecialchars($row['id']); ?>" class="delete">Excluir</a>
+                                <a href="index.php?delete=<?php echo htmlspecialchars($row['id']); ?>">
+                                <button class="button">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 69 14"
+                                    class="svgIcon bin-top"
+                                >
+                                    <g clip-path="url(#clip0_35_24)">
+                                    <path
+                                        fill="black"
+                                        d="M20.8232 2.62734L19.9948 4.21304C19.8224 4.54309 19.4808 4.75 19.1085 4.75H4.92857C2.20246 4.75 0 6.87266 0 9.5C0 12.1273 2.20246 14.25 4.92857 14.25H64.0714C66.7975 14.25 69 12.1273 69 9.5C69 6.87266 66.7975 4.75 64.0714 4.75H49.8915C49.5192 4.75 49.1776 4.54309 49.0052 4.21305L48.1768 2.62734C47.3451 1.00938 45.6355 0 43.7719 0H25.2281C23.3645 0 21.6549 1.00938 20.8232 2.62734ZM64.0023 20.0648C64.0397 19.4882 63.5822 19 63.0044 19H5.99556C5.4178 19 4.96025 19.4882 4.99766 20.0648L8.19375 69.3203C8.44018 73.0758 11.6746 76 15.5712 76H53.4288C57.3254 76 60.5598 73.0758 60.8062 69.3203L64.0023 20.0648Z"
+                                    ></path>
+                                    </g>
+                                    <defs>
+                                    <clipPath id="clip0_35_24">
+                                        <rect fill="white" height="14" width="69"></rect>
+                                    </clipPath>
+                                    </defs>
+                                </svg>
+
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 69 57"
+                                    class="svgIcon bin-bottom"
+                                >
+                                    <g clip-path="url(#clip0_35_22)">
+                                    <path
+                                        fill="black"
+                                        d="M20.8232 -16.3727L19.9948 -14.787C19.8224 -14.4569 19.4808 -14.25 19.1085 -14.25H4.92857C2.20246 -14.25 0 -12.1273 0 -9.5C0 -6.8727 2.20246 -4.75 4.92857 -4.75H64.0714C66.7975 -4.75 69 -6.8727 69 -9.5C69 -12.1273 66.7975 -14.25 64.0714 -14.25H49.8915C49.5192 -14.25 49.1776 -14.4569 49.0052 -14.787L48.1768 -16.3727C47.3451 -17.9906 45.6355 -19 43.7719 -19H25.2281C23.3645 -19 21.6549 -17.9906 20.8232 -16.3727ZM64.0023 1.0648C64.0397 0.4882 63.5822 0 63.0044 0H5.99556C5.4178 0 4.96025 0.4882 4.99766 1.0648L8.19375 50.3203C8.44018 54.0758 11.6746 57 15.5712 57H53.4288C57.3254 57 60.5598 54.0758 60.8062 50.3203L64.0023 1.0648Z"
+                                    ></path>
+                                    </g>
+                                    <defs>
+                                    <clipPath id="clip0_35_22">
+                                        <rect fill="white" height="57" width="69"></rect>
+                                    </clipPath>
+                                    </defs>
+                                </svg>
+                                </button></a>
                             </div>
                         </form>
                     </td>
