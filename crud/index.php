45e58fd1c43aaf8bfd882b1a1dcf4f5d8a1b2339
henrique0927 <henrique_stolle@estudante.sesisenai.org.br>
2024-09-23 11:11:10 -0300

    conexao com a base de dados

--- crud/index.php
+++ crud/index.php
@@ -1,17 +1,16 @@
-<?php
-include "database.php";
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>CRUD em Dupla</title>
-    <link rel="stylesheet" href="./css/style.css">
-</head>
-<body>
-    
-</body>
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+  <body>
+    <main>
+      <div class="form">
+        <form action="">aa</form>
+      </div>
+    </main>
+  </body>
 </html>
