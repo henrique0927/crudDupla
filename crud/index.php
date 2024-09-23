dfbaa08bdce622178d1b78649ca8ee6080000143
henrique0927 <henrique_stolle@estudante.sesisenai.org.br>
2024-09-23 12:02:47 -0300

    atualizado o index, estilo

--- crud/index.php
+++ crud/index.php
@@ -15,30 +15,8 @@ include "database.php";
         
         <!-- formulário para criar novo pedido -->
         <form action="index.php" method="POST">
-            <!-- Selecionar professor -->
-            <select name="professor_id" required>
-                <option value="">Selecione o Professor</option>
-                <?php
-                $sql_professores = "SELECT * FROM professores";
-                $result_professores = $conn->query($sql_professores);
-                while ($professor = $result_professores->fetch_assoc()):
-                ?>
-                    <option value="<?php echo $professor['id']; ?>"><?php echo htmlspecialchars($professor['nome']); ?></option>
-                <?php endwhile; ?>
-            </select>
-            
-            <!-- Selecionar sala -->
-            <select name="aula_id" required>
-                <option value="">Selecione a Sala</option>
-                <?php
-                $sql_aulas = "SELECT * FROM aulas";
-                $result_aulas = $conn->query($sql_aulas);
-                while ($aula = $result_aulas->fetch_assoc()):
-                ?>
-                    <option value="<?php echo $aula['id']; ?>"><?php echo htmlspecialchars($aula['sala']); ?></option>
-                <?php endwhile; ?>
-            </select>
-            
+            <input type="text" name="nome_professor" placeholder="Nome do Professor" required>
+            <input type="text" name="sala" placeholder="Sala" required>
             <input type="datetime-local" name="hora_aula" required>
             <button type="submit" name="create">Criar Pedido</button>
         </form>
@@ -73,33 +51,8 @@ include "database.php";
                         <!-- formulário para atualizar pedido -->
                         <form action="index.php" method="POST" style="display:inline; margin:auto;">
                             <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>">
-                            
-                            <!-- Selecionar novo professor -->
-                            <select name="professor_id" required>
-                                <?php
-                                $sql_professores = "SELECT * FROM professores";
-                                $result_professores = $conn->query($sql_professores);
-                                while ($professor = $result_professores->fetch_assoc()):
-                                ?>
-                                    <option value="<?php echo $professor['id']; ?>" <?php echo $professor['id'] == $row['professor_id'] ? 'selected' : ''; ?>>
-                                        <?php echo htmlspecialchars($professor['nome']); ?>
-                                    </option>
-                                <?php endwhile; ?>
-                            </select>
-                            
-                            <!-- Selecionar nova sala -->
-                            <select name="aula_id" required>
-                                <?php
-                                $sql_aulas = "SELECT * FROM aulas";
-                                $result_aulas = $conn->query($sql_aulas);
-                                while ($aula = $result_aulas->fetch_assoc()):
-                                ?>
-                                    <option value="<?php echo $aula['id']; ?>" <?php echo $aula['id'] == $row['aula_id'] ? 'selected' : ''; ?>>
-                                        <?php echo htmlspecialchars($aula['sala']); ?>
-                                    </option>
-                                <?php endwhile; ?>
-                            </select>
-                            
+                            <input type="text" name="nome_professor" value="<?php echo htmlspecialchars($row['nome_professor']); ?>" required>
+                            <input type="text" name="sala" value="<?php echo htmlspecialchars($row['nome_sala']); ?>" required>
                             <input type="datetime-local" name="hora_aula" value="<?php echo htmlspecialchars(date('Y-m-d\TH:i', strtotime($row['hora_aula']))); ?>" required>
                             
                             <div id="botao-junto">
@@ -120,32 +73,57 @@ include "database.php";
 <?php
 // Lógica para criação
 if (isset($_POST['create'])) {
-    $professor_id = $_POST['professor_id'];
-    $aula_id = $_POST['aula_id'];
+    $nome_professor = $_POST['nome_professor'];
+    $sala = $_POST['sala'];
     $hora_aula = $_POST['hora_aula'];
 
-    $sql_insert = "INSERT INTO dia_hora (professor_id, aula_id, hora_aula) VALUES ('$professor_id', '$aula_id', '$hora_aula')";
-    $conn->query($sql_insert);
+    // Inserir novo professor na tabela "professores"
+    $sql_insert_professor = "INSERT INTO professores (nome) VALUES ('$nome_professor')";
+    $conn->query($sql_insert_professor);
+    $professor_id = $conn->insert_id;
+
+    // Inserir nova sala na tabela "aulas"
+    $sql_insert_aula = "INSERT INTO aulas (sala) VALUES ('$sala')";
+    $conn->query($sql_insert_aula);
+    $aula_id = $conn->insert_id;
+
+    // Inserir na tabela "dia_hora" o relacionamento entre o professor, a sala e a hora da aula
+    $sql_insert_dia_hora = "INSERT INTO dia_hora (professor_id, aula_id, hora_aula) VALUES ('$professor_id', '$aula_id', '$hora_aula')";
+    $conn->query($sql_insert_dia_hora);
+
     header("Location: index.php");
 }
 
 // Lógica para atualização
 if (isset($_POST['update'])) {
     $id = $_POST['id'];
-    $professor_id = $_POST['professor_id'];
-    $aula_id = $_POST['aula_id'];
+    $nome_professor = $_POST['nome_professor'];
+    $sala = $_POST['sala'];
     $hora_aula = $_POST['hora_aula'];
 
-    $sql_update = "UPDATE dia_hora SET professor_id='$professor_id', aula_id='$aula_id', hora_aula='$hora_aula' WHERE id='$id'";
-    $conn->query($sql_update);
+    // Atualizar o nome do professor na tabela "professores"
+    $sql_update_professor = "UPDATE professores SET nome='$nome_professor' WHERE id=(SELECT professor_id FROM dia_hora WHERE id='$id')";
+    $conn->query($sql_update_professor);
+
+    // Atualizar o nome da sala na tabela "aulas"
+    $sql_update_aula = "UPDATE aulas SET sala='$sala' WHERE id=(SELECT aula_id FROM dia_hora WHERE id='$id')";
+    $conn->query($sql_update_aula);
+
+    // Atualizar a hora da aula na tabela "dia_hora"
+    $sql_update_dia_hora = "UPDATE dia_hora SET hora_aula='$hora_aula' WHERE id='$id'";
+    $conn->query($sql_update_dia_hora);
+
     header("Location: index.php");
 }
 
 // Lógica para exclusão
 if (isset($_GET['delete'])) {
     $id = $_GET['delete'];
+    
+    // Remover a entrada da tabela "dia_hora"
     $sql_delete = "DELETE FROM dia_hora WHERE id='$id'";
     $conn->query($sql_delete);
+
     header("Location: index.php");
 }
 ?>
