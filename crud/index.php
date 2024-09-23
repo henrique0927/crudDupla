8385a0518e0d6832bb0c77d00ce8007fb513038c
henrique0927 <henrique_stolle@estudante.sesisenai.org.br>
2024-09-23 11:34:43 -0300

    arrrumando problemas de banco e começando o front

--- crud/index.php
+++ crud/index.php
@@ -1,3 +1,8 @@
+<?php
+include "database.php";
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -7,10 +12,63 @@
     <link rel="stylesheet" href="./css/style.css" />
   </head>
   <body>
-    <main>
-      <div class="form">
-        <form action=""></form>
-      </div>
-    </main>
+  <div class="container">
+        <h1>Sistema de Pedidos</h1>
+        
+        <!-- formulário para criar novo pedido -->
+        <form action="index.php" method="POST">
+            <input type="text" name="nome_cliente" placeholder="Nome do Cliente" required>
+            <input type="text" name="nome_produto" placeholder="Nome do Produto" required>
+            <input type="number" name="quantidade" placeholder="Quantidade" required>
+            <input type="date" name="data_pedido" placeholder="Data do Pedido" required>
+            <button type="submit" name="create">Criar Pedido</button>
+        </form>
+
+        <!-- lista de pedidos -->
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome do Cliente</th>
+                    <th>Nome do Produto</th>
+                    <th>Quantidade</th>
+                    <th>Data do Pedido</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                // puxar os pedidos "READ"
+                $sql = "SELECT * FROM pedidos";
+                $result = $conn->query($sql);
+                while ($row = $result->fetch_assoc()):
+                ?>
+                <tr>
+                    <td class="td-data"><?php echo htmlspecialchars($row['id']); ?></td>
+                    <td class="td-data"><?php echo htmlspecialchars($row['nome_cliente']); ?></td>
+                    <td class="td-data"><?php echo htmlspecialchars($row['nome_produto']); ?></td>
+                    <td class="td-data"><?php echo htmlspecialchars($row['quantidade']); ?></td>
+                    <td class="data-pedido"><?php echo htmlspecialchars($row['data_pedido']); ?></td>
+                    <td>
+                        <!-- formulário para atualizar pedido -->
+                        <form action="index.php" method="POST" style="display:inline; margin:auto;">
+                            <input type="hidden" name="id" value="<?php echo htmlspecialchars($row['id']); ?>">
+                            <input type="text" name="nome_cliente" value="<?php echo htmlspecialchars($row['nome_cliente']); ?>" required>
+                            <input type="text" name="nome_produto" value="<?php echo htmlspecialchars($row['nome_produto']); ?>" required>
+                            <input type="number" name="quantidade" value="<?php echo htmlspecialchars($row['quantidade']); ?>" required>
+                            <input type="date" name="data_pedido" value="<?php echo htmlspecialchars($row['data_pedido']); ?>" required>
+                            <div id="botao-junto">
+                                <button type="submit" name="update">Atualizar</button>
+                                <!-- botão para excluir pedido -->
+                                <a href="index.php?delete=<?php echo htmlspecialchars($row['id']); ?>" class="delete">Excluir</a>
+                            </div>
+                            
+                        </form>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
+    </div>
   </body>
 </html>
